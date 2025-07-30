2edeb310b4c01b597e19efe6552c72a799c7e558
juyoung <juyoungjeong.797@gmail.com>
2025-07-30 10:22:34 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="card">
+        <img src="./avatar.jpg" alt="avatar" />
+        <h2>j797</h2>
+        <h2>Programmer</h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
