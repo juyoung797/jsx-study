1c1b8a9f286b7be491fe8baa28ccb9ebfbc2147c
juyoung <juyoungjeong.797@gmail.com>
2025-07-30 11:22:50 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import "./App.css";
+import UserCard from "./components/UserCard";
+
+const userData = [
+  {
+    name: "정주영",
+    job: "baeksu",
+    imageSrc: "avatar.jpg",
+  },
+  {
+    name: "가나디",
+    job: "baeksu",
+    imageSrc: "avatar.jpg",
+  },
+  {
+    name: "요를레이히",
+    job: "baeksu",
+    imageSrc: "avatar.jpg",
+  },
+]
 
 const App = () => {
   return (
     <>
-      <div className="card">
-        <img src="./avatar.jpg" alt="avatar" />
-        <h2>797</h2>
-        <h2>Programmer</h2>
-      </div>
+      <UserCard 
+        name={userData[0].name} 
+        job={userData[0].job} 
+        imageSrc={userData[0].imageSrc} 
+      />
+      <UserCard 
+        name={userData[1].name} 
+        job={userData[1].job} 
+        imageSrc={userData[1].imageSrc} 
+      />
+      <UserCard 
+        name={userData[2].name} 
+        job={userData[2].job} 
+        imageSrc={userData[2].imageSrc} 
+      />
     </>
   );
 };
