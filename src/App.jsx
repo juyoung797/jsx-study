18f97e4af76dd0f16c3beceb9373690fa0e497ea
juyoung <juyoungjeong.797@gmail.com>
2025-07-30 10:43:03 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <>
       <div className="card">
         <img src="./avatar.jpg" alt="avatar" />
-        <h2>j797</h2>
+        <h2>797</h2>
         <h2>Programmer</h2>
       </div>
     </>
